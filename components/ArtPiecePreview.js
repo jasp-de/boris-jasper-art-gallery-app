1a2018eb720695dae38bd51f4773e1a9fcc18ab7
jasp_de <d.j.deppermann@gmail.com>
2024-10-21 15:12:55 +0200

    colors and some styling

--- components/ArtPiecePreview.js
+++ components/ArtPiecePreview.js
@@ -2,22 +2,36 @@ import Link from "next/link";
 import Head from "next/head";
 import Router from "next/router";
 
-export default function ArtPiecePreview({ pieces }) {
-  console.log(pieces);
-
+export default function ArtPiecePreview({ pieces, handleFavorites }) {
   if (!pieces) {
     return <p>Loading...</p>;
   }
 
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: "red",
+        padding: "50px",
+        margin: "20px",
+      }}
+    >
       {pieces.map(({ slug, imageSource, name, artist }) => (
-        <div key={slug}>
+        <div
+          key={slug}
+          style={{
+            backgroundColor: "#f9f9f9",
+            padding: "50px",
+            margin: "20px",
+          }}
+        >
           <Link slug={slug} href={`/art-pieces/${slug}`}>
             <img src={imageSource} alt={name} width="400px" />
           </Link>
           <p>{name}</p>
           <p>{artist}</p>
+          <button className="FavoritesButton" onClick="handleFavorites">
+            TOGGLE FAVORITES
+          </button>
         </div>
       ))}
     </div>
