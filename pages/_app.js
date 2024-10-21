fa8a6163f0545762f793e9feadf36aa3e7a9c491
Borys Kravtsov <mamkinboris3@gmail.com>
2024-10-21 15:03:11 +0200

    spotlight component done

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,39 @@
 import GlobalStyle from "../styles";
-
+import useSWR from "swr";
+import ArtPieces from "@/components/ArtPieces";
+import Spotlight from "@/components/Spotlight";
+import { useEffect, useState } from "react";
+const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function App({ Component, pageProps }) {
+  const { data, error } = useSWR(
+    `https://example-apis.vercel.app/api/art`,
+    fetcher
+  );
+
+  const [randomValueSpotlight, setRandomValueSpotlight] = useState(null);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setRandomValueSpotlight(Math.floor(Math.random() * data.length));
+    }
+  }, [data]);
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error fetching data!</div>;
+  }
+
   return (
     <>
+      {randomValueSpotlight !== null && (
+        <Spotlight
+          artist={data[randomValueSpotlight]}
+          image={data[randomValueSpotlight]}
+        />
+      )}
+      <ArtPieces pieces={data} />
       <GlobalStyle />
       <Component {...pageProps} />
     </>
