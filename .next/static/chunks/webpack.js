714ea55074f75c2ccbb9d715c0a8618a591263b8
jasp_de <d.j.deppermann@gmail.com>
2024-10-21 12:11:02 +0200

    ...

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "13a92b283119e627"; }
+/******/ 		__webpack_require__.h = function() { return "a6567a2ccecabb8c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import useSWR from "swr";
 import { useImmerLocalStorageState } from "../lib/hook/useImmerLocalStorageState";
 import ArtPieces from "@/components/ArtPieces";
-import ArtPiecePreview from "@/components/ArtPiecePreview";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
@@ -22,7 +21,6 @@ export default function HomePage() {
     <div>
       <h1>Hello from Next.jspr</h1>
       <ArtPieces pieces={data} />
-      <ArtPiecePreview pieces={data} />
     </div>
   );
 }
