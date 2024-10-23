0be953111de82cb469243b7d73869e2e2e27e28f
jasp_de <d.j.deppermann@gmail.com>
2024-10-23 10:36:27 +0200

    nav links

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "004425e76c9f7d21"; }
+/******/ 		__webpack_require__.h = function() { return "b594c69dada3259e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
