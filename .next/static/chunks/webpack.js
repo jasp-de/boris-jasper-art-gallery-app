07a80d00f8dd979ee0d9754c37bd238515ad4471
jasp_de <d.j.deppermann@gmail.com>
2024-10-23 14:26:41 +0200

    ..

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d8401587021ac3e"; }
+/******/ 		__webpack_require__.h = function() { return "b7f38c90dbfd91a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import { useImmerLocalStorageState } from "../lib/hook/useImmerLocalStorageState";
-<<<<<<< HEAD
-=======
 import ArtPieces from "@/components/ArtPieces";
 import ArtPiecePreview from "@/components/ArtPiecePreview";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
->>>>>>> main
 
 export default function HomePage() {
   const [artPiecesInfo, updateArtPiecesInfo] = useImmerLocalStorageState(
